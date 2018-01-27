37fa03989b99e5ff7e98d13e7a14efff9fd5ff35
Hekovi <hclaudelin@gmail.com>
2018-01-27 11:01:15 +0200

    Controls - Pooled input messages and made it possible that their origin is moving (satellite for example)

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -6,11 +6,12 @@ public class SpaceshipController : MonoBehaviour
 {
 	public class MessageData
 	{
+		public bool inUse;
+		public Vector3 creationPosition;
+		public float range;
+		// Payload: (Could be bitflags :))
 		public bool left;
 		public bool right;
-		public float creationTime;
-		public float range;
-		public bool inUse;
 	};
 
 	[SerializeField] private Transform earthTransform;
@@ -21,13 +22,17 @@ public class SpaceshipController : MonoBehaviour
 
 	private float realTimePassed;
 	private float distanceToEarthSqr;
-	private List<MessageData> messageBuffer = new List<MessageData>(1000);
-	//private int messageBufferCounter = 0;
+	private MessageData[] messageBuffer = new MessageData[200];
+	private int messageBufferCounter = 0;
+	private int messagesInUseCount;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		for (int i = 0; i < messageBuffer.Length; i++)
+		{
+			messageBuffer[i] = new MessageData();
+		}
 	}
 	
 	// Update is called once per frame
@@ -37,26 +42,37 @@ public class SpaceshipController : MonoBehaviour
 
 		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
 		{
-			MessageData message = new MessageData()
+			if(messageBuffer[messageBufferCounter].inUse)
 			{
-				left = Input.GetKey(KeyCode.LeftArrow),
-				right = Input.GetKey(KeyCode.RightArrow),
-				creationTime = realTimePassed,
-				range = initialMessageRange,
-				inUse = true
-			};
-			messageBuffer.Add(message);
+				Debug.LogWarning("Buffer overran! Increase it's size pls");
+			}
+			else
+			{
+				messageBuffer[messageBufferCounter].left = Input.GetKey(KeyCode.LeftArrow);
+				messageBuffer[messageBufferCounter].right = Input.GetKey(KeyCode.RightArrow);
+				messageBuffer[messageBufferCounter].range = initialMessageRange;
+				messageBuffer[messageBufferCounter].creationPosition = earthTransform.position;
+				messageBuffer[messageBufferCounter].inUse = true;
+				messageBufferCounter++;
+				if(messageBufferCounter == messageBuffer.Length)
+				{
+					messageBufferCounter = 0;
+				}
+			}
 		}
 		
-		// Process input queue
-		distanceToEarthSqr = (transform.position - earthTransform.position).sqrMagnitude;
-		for (int i = 0; i < messageBuffer.Count; i++)
+		// Process message queue:
+		messagesInUseCount = 0;
+		for (int i = 0; i < messageBuffer.Length; i++)
 		{
 			if (messageBuffer[i].inUse)
 			{
+				messagesInUseCount++;
 				DrawMessage(messageBuffer[i]);
+
 				messageBuffer[i].range += (realTimePassed * messageExpansionSpeed);
-				if (distanceToEarthSqr <= (messageBuffer[i].range * messageBuffer[i].range))
+				float distanceSqr = (transform.position - messageBuffer[i].creationPosition).sqrMagnitude;
+				if (distanceSqr <= (messageBuffer[i].range * messageBuffer[i].range))
 				{
 					ProcessMessage(messageBuffer[i]);
 				}
@@ -65,6 +81,8 @@ public class SpaceshipController : MonoBehaviour
 
 		// Move spaceship:
 		this.transform.position += (transform.up * spaceshipSpeed);
+
+		//Debug.Log("Messages in use: " + messagesInUseCount);
 	}
 
 	private void ProcessMessage(MessageData msg)
@@ -84,7 +102,7 @@ public class SpaceshipController : MonoBehaviour
 	private void DrawCircle(Vector3 position, float range, Color color, int sides = 16)
 	{
 		Vector3 arm = Vector3.up * range;
-		Quaternion q = Quaternion.AngleAxis(360.0f / 16, Vector3.forward);
+		Quaternion q = Quaternion.AngleAxis(360.0f / sides, Vector3.forward);
 		for (int i = 0; i < sides; i++)
 		{
 			Vector3 lastArm = arm;
