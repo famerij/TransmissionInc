7789d5849737927fbd9b3c2df2e57fdad0fb8c39
Henri <henri.viitanen@gmail.com>
2018-01-27 23:36:28 +0200

    Added animation when entering FastGate

--- Assets/Scripts/FastGate.cs
+++ Assets/Scripts/FastGate.cs
@@ -1,19 +1,51 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class FastGate : Collectible
 {
 	[SerializeField] private string destinationSceneName = "INSERT";
 	[SerializeField] private float delayBeforeJump = 2.0f;
+	[SerializeField] private Transform animationRoot;
+	[SerializeField] private Transform spaceShip;
+	[SerializeField] private Animation animation;
 
 	public string DestinationSceneName {  get { return destinationSceneName; } }
 	public float DelayBeforeJump { get { return delayBeforeJump; } }
 
+	private Collider2D collider;
+
+	void Start()
+	{
+		collider = GetComponent<Collider2D>();
+	}
+	
+	[ContextMenu("Collect")]
 	public override void Collect()
 	{
+		collider.enabled = false;
+		StartCoroutine(MoveSpaceShipToAnimationRoot());
 		Debug.Log("Fastgate collected!");
-		audioSource.PlayOneShot(soundClip);
+	}
+
+	IEnumerator MoveSpaceShipToAnimationRoot()
+	{
+		Camera.main.GetComponent<CinemachineVirtualCamera>().enabled = false;
+		Debug.Log("Starting animation");
+		animation.Play();
+		audioSource.PlayOneShot(soundClip);	
+		
+		float duration = 1f;
+		float t = 0f;
+		while (t < 1f)
+		{
+			t += Time.deltaTime / duration;
+			spaceShip.transform.localPosition =
+				Vector3.Lerp(spaceShip.transform.position, animationRoot.position, t);
+			yield return null;
+		}
+		spaceShip.SetParent(animationRoot, true);
 	}
 }
