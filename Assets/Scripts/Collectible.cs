60829c87681c8b10da272d84552a8361f1e86143
Henri <henri.viitanen@gmail.com>
2018-01-27 18:15:53 +0200

    Added collectible sound

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -7,13 +7,19 @@ public class Collectible : MonoBehaviour
 {
 	[SerializeField] private float collectionRange = 2.0f;
 
+	[Header("Audio")]
+	[SerializeField] private AudioClip soundClip;
+
 	private LevelManager levelManager;
+	private AudioSource audioSource;
+	
 	public float CollectionRange {  get { return collectionRange; } }
 
 	protected void OnEnable()
 	{
 		levelManager = FindObjectOfType<LevelManager>();
 		levelManager.AddCollectible(this);
+		audioSource = GetComponent<AudioSource>();
 	}
 
 	protected void Update()
@@ -27,6 +33,7 @@ public class Collectible : MonoBehaviour
 		Debug.Log("Collected!");
 		Destroy(gameObject);
 		//TODO: Fire off effects, sounds etc.
+		audioSource.PlayOneShot(soundClip);
 	}
 
 }
