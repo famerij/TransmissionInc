7331be9e31433a51180a51aea95e8d26efe87276
Ari Arnbjörnsson <ari@flassari.is>
2018-01-27 21:27:06 +0200

    Add prologue part

---
+++ Assets/Scripts/PrologueLevelController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrologueLevelController : MonoBehaviour
+{
+	public Ship ship;
+	public MessageTransmitter transmitter;
+	public FaderInOut levelEndFadeOut;
+	public string nextLevel;
+
+	public GameObject[] disableOnStartObjects;
+
+	public void Start()
+	{
+		transmitter.enabled = false;
+		ship.enabled = false;
+
+		foreach (var go in disableOnStartObjects)
+		{
+			go.SetActive(false);
+		}
+
+		StartCoroutine(Spin());
+	}
+
+	private IEnumerator Spin()
+	{
+		Transform shipTransform = ship.transform;
+		float xSpeed = 0.3f;
+
+		while (true)
+		{
+			// Slow down until stop
+			xSpeed *= 0.999f;
+
+			shipTransform.Rotate(0, 0, -Time.deltaTime * 250);
+			shipTransform.position = new Vector3(shipTransform.position.x + xSpeed * Time.deltaTime,
+				shipTransform.position.y, shipTransform.position.z);
+			yield return null;
+		}
+	}
+
+	public void OnLevelEndFadeOutComplete()
+	{
+		StartCoroutine(WaitThenEndLevel());
+	}
+
+	public IEnumerator WaitThenEndLevel()
+	{
+		yield return new WaitForSeconds(2);
+		SceneManager.LoadScene(nextLevel);
+	}
+}
