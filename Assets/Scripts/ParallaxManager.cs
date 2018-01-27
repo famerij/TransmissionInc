27593d12a2bc8a2cf5ccf9336e14c516900824a6
Henri <henri.viitanen@gmail.com>
2018-01-27 18:45:10 +0200

    Added parallaxmanager, use MainCamera prefab for a starting point on building background for levels

---
+++ Assets/Scripts/ParallaxManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ParallaxManager : MonoBehaviour
+{
+	[SerializeField] private Transform Far;
+	[SerializeField] private float FarSpeed;
+	[SerializeField] private Transform Near;
+	[SerializeField] private float NearSpeed;
+	[SerializeField] private Transform Nearest;
+	[SerializeField] private float NearestSpeed;
+
+	private Vector3 previousPosition;
+	
+	void Update()
+	{
+		var delta = transform.position - previousPosition;
+
+		Far.transform.position -= delta * FarSpeed * Time.deltaTime;
+		Near.transform.position -= delta * NearSpeed * Time.deltaTime;
+		Nearest.transform.position -= delta * NearestSpeed * Time.deltaTime;
+		
+		previousPosition = transform.position;
+	}
+}
