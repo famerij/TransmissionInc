323cc16539a7381053b4dcf053b618626496232c
Ari Arnbjörnsson <ari@flassari.is>
2018-01-30 19:12:46 +0200

    Add gameDebug for easier presentations

---
+++ Assets/Scripts/GameDebug.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GameDebug : MonoBehaviour
+{
+	void Update ()
+	{
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			SceneManager.LoadScene("SplashScreen");
+		}
+		
+		if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+		{
+			int fromKey = (int)KeyCode.Alpha1;
+			string[] levels = new string[] {
+				"0_0Tutorial",
+				"0_1Prologue",
+				"1_Level1",
+				"2_Level2",
+				"3_Level3",
+				"4_Epilogue"
+			};
+
+			for (int i = fromKey; i < fromKey + levels.Length; i++)
+			{
+				if (Input.GetKeyDown((KeyCode)i))
+				{
+					SceneManager.LoadScene(levels[i - fromKey]);
+					return;
+				}
+			}
+		}
+	}
+}
